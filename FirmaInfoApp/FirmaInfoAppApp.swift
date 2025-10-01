597560d16c23888853b5f5b70e23dd1c532a57d9
Kavz44 <kavingregary@outlook.com>
2025-10-01 20:42:29 +0800

    Initial Commit

---
+++ FirmaInfoApp/FirmaInfoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirmaInfoAppApp.swift
+//  FirmaInfoApp
+//
+//  Created by Kavin Gregary Anand on 01/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct FirmaInfoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
