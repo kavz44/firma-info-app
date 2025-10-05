25577ae0007e834cfc5ff37000be5796155181d0
Kavz44 <kavingregary@outlook.com>
2025-10-05 21:42:07 +0800

    the whole searchbar broke, fix next time

--- FirmaInfoApp/views/MainMenuView.swift
+++ FirmaInfoApp/views/MainMenuView.swift
@@ -13,19 +13,56 @@ struct MainMenuView: View {
     
     var body: some View {
         
-        ZStack{
-            Image("backround")
-                .resizable()
-                .scaledToFill()
-                .ignoresSafeArea()
+        NavigationStack{
+            ZStack{
+                Image("backround")
+                    .resizable()
+                    .scaledToFill()
+                    .ignoresSafeArea()
+                
+                if (!viewModel.forslag.isEmpty) {
+                    
+                    ScrollView{
+                        VStack(alignment: .leading){
+                            ForEach(viewModel.forslag) {firma in
+                                Button(action: {
+                                    viewModel.velgForslag(firma: firma)
+                                }){
+                                    VStack(alignment: .leading){
+                                        if #available(iOS 26.0, *) {
+                                            Text("Firmanavn: \(firma.navn)")
+                                                .glassEffect()
+                                        } else {
+                                            // Fallback on earlier versions
+                                            
+                                            // Text("Orgnr: \(firma.orgnummer)")
+                                            // if let adresse = firma.forretningsadresse?.poststed {
+                                            // Text("Adresse: \(adresse)")
+                                            // }
+                                        }
+                                    }
+                                    
+                                }
+                                .buttonStyle(PlainButtonStyle())
+                            }
+                            
+                        }
+                        
+                    }
+                    
+                    
+                }
+                
+            }
+        } // navigationStack end
+        .searchable(text: $viewModel.query)
+        .searchSuggestions{
+            ForEach(viewModel.forslag) { firma in
+                Text("\(firma.navn)")
+            }
             
-            FirmaAutocomplete()
         }
     }
-    
-    
-    
-    
 }
 
 
--- FirmaInfoApp/views/viewUtilities/FirmaAutoComplete.swift
+++ FirmaInfoApp/views/viewUtilities/FirmaAutoComplete.swift
@@ -11,45 +11,7 @@ struct FirmaAutocomplete: View {
     @StateObject private var viewModel = FirmaViewModel()
     
     var body: some View {
-        NavigationStack{
-            VStack {
-                
-                // Dropdown med forslag
-                if !viewModel.forslag.isEmpty {
-                    ScrollView {
-                        VStack(alignment: .leading, spacing: 0) {
-                            ForEach(viewModel.forslag) { firma in
-                                Button(action: {
-                                    viewModel.velgForslag(firma: firma)
-                                }) {
-                                    VStack(alignment: .leading) {
-                                        Text(firma.navn).bold()
-                                        Text("Org.nr: \(firma.orgnummer)")
-                                            .font(.subheadline)
-                                            .foregroundColor(.gray)
-                                        if let adresse = firma.forretningsadresse?.poststed {
-                                            Text(adresse).font(.subheadline)
-                                        }
-                                    }
-                                    .padding(8)
-                                    .background(Color.white)
-                                }
-                                .buttonStyle(PlainButtonStyle())
-                                Divider()
-                            }
-                        }
-                        .background(Color(.systemGray6))
-                        .cornerRadius(8)
-                        .shadow(radius: 3)
-                    }
-                    .frame(maxHeight: 300)
-                }
-                
-                Spacer()
-            }
-            .padding()
-        } // NavigationStach end
-        .searchable(text: $viewModel.query)
+        
     }
 }
 
